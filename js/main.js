07459983ead8d8566a61985e01e688894b1a8750
yash101 <lodhad@gmail.com>
2019-04-23 11:58:14 -0700

    Creating shitty backend

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@
 
 	window.links.forceLoad = function(loc) {
 		var uri = window.location.origin + window.location.pathname + "?shared_uri=" + loc;
-		window.location.href = uri;
+		shared_code = decodeURI(loc);
+		window.load(shared_code);
 	};
 	window.setGridSize = function setsize(x, y) {
 		window.initialized = true;
---
+++ share.php
@@ -0,0 +1,11 @@
+<?php
+$tm = time();
+$fil = "shared_code/" . string($tm) . ".shared.txt";
+if (strlen($_REQUEST["shared_code"] > 16384)) {
+	die("File too large");
+}
+$fptr = fopen($fil, "w") or die("Unable to create share!");
+fwrite($fptr, $_REQUEST["shared_code"]);
+fclose($fptr);
+echo $tm;
+?>
